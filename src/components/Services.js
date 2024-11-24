f27332e4c7f533386eec3c016bfb8ff752204eec
Matt0107 <mathieu.durimel@gmail.com>
2024-11-24 19:42:07 +0100

    final version 1

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FaDumbbell, FaCalendarAlt, FaHeartbeat } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 import '../styles/Services.css';
 
 function Services() {
     const { t } = useTranslation();
+    const sectionRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                }
+            },
+            { threshold: 0.4 } // Trigger when 30% of the section is visible
+        );
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+    }, []);
 
     return (
-        <section className="services-section" id="services-section">
+        <section
+            className={`services-section ${isVisible ? 'animate-icons' : ''}`}
+            id="services-section"
+            ref={sectionRef}
+        >
             {/* Première partie avec le fond en filigrane */}
             <div className="services-intro">
                 <h2 className="services-title">{t('services.title')}</h2>
                 <p className="services-description">{t('services.description')}</p>
             </div>
-            
+
             {/* Deuxième partie avec les icônes de services */}
             <div className="services-grid">
                 <div className="service">
-                <FaDumbbell className="service-icon" />
+                    <FaDumbbell className="service-icon" />
                     <h3 className="service-title">{t('services.timing.title')}</h3>
                     <p className="service-description">{t('services.timing.description')}</p>
                 </div>
